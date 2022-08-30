ed67b7464b67f3b0949c9d4f5e2d30c4d3f3b3a3
raphael <raph123411@gmail.com>
2022-08-30 10:46:31 -0400

    ProjectFinal

--- main.py
+++ main.py
@@ -3,14 +3,23 @@
 # Press Maj+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+prices = [310, 315, 275, 295, 260, 270, 290, 230, 255, 250]
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+profit = 0
+bought = 0
+sold = 0
+
+for buy in prices:
+    boughtTemp = buy
+    for sell in prices[prices.index(buy):]:
+        soldTemp = sell
+        profitTemp = soldTemp - boughtTemp
+        if profitTemp > profit:
+            profit = profitTemp
+            bought = boughtTemp
+            sold = soldTemp
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("Bought : " + str(bought))
+print("Sold : " + str(sold))
+print("Profit : " + str(profit))
